31417aadb4b60f30a148f6e0bb44fea6d2147403
Sashalu2001 <61570119+Sashalu2001@users.noreply.github.com>
2020-11-04 14:08:06 +1000

    Paint

---
+++ MyPaint/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace MyPaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выйтиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оПрограммеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Menu;
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 80);
+            this.panel1.TabIndex = 0;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(672, 42);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(116, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Масштабирование";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(672, 10);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(116, 23);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Рука";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(217, 9);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(97, 24);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Карандаш";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(320, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 24);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Эллипс";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(217, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(97, 24);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Прямоугольник";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(132, 9);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ShortcutsEnabled = false;
+            this.textBox2.Size = new System.Drawing.Size(54, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TabStop = false;
+            this.textBox2.Text = "Толщина";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(107, 35);
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 9);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ShortcutsEnabled = false;
+            this.textBox1.Size = new System.Drawing.Size(89, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = "Текущий цвет";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(12, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 30);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem,
+            this.оПрограммеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выйтиToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // выйтиToolStripMenuItem
+            // 
+            this.выйтиToolStripMenuItem.Name = "выйтиToolStripMenuItem";
+            this.выйтиToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.выйтиToolStripMenuItem.Text = "Выйти";
+            this.выйтиToolStripMenuItem.Click += new System.EventHandler(this.выйтиToolStripMenuItem_Click);
+            // 
+            // оПрограммеToolStripMenuItem
+            // 
+            this.оПрограммеToolStripMenuItem.Name = "оПрограммеToolStripMenuItem";
+            this.оПрограммеToolStripMenuItem.Size = new System.Drawing.Size(94, 20);
+            this.оПрограммеToolStripMenuItem.Text = "О программе";
+            this.оПрограммеToolStripMenuItem.Click += new System.EventHandler(this.оПрограммеToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 104);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 346);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "MyPaint";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выйтиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оПрограммеToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ MyPaint/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyPaint
+{
+    public partial class Form1 : Form
+    {
+        Bitmap pic;
+        Bitmap t_pic;
+        string mode;
+        int x1, y1;
+        int xclick1, yclick1;
+        int c_c;
+        double width, height, scale;
+        public Form1()
+        {
+            InitializeComponent();
+            mode = "Карандаш";
+            pic = new Bitmap(1366, 640);
+            t_pic = new Bitmap(1366, 640);
+            x1 = y1 = 0;
+            c_c = 4;
+            width = 1366;
+            height = 640;
+            scale = Math.Min(width / pic.Width, height / pic.Height);
+            colorDialog1.FullOpen = true;
+            colorDialog1.Color = this.button1.BackColor;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button b = (Button)sender;
+            if (colorDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            this.button1.BackColor = colorDialog1.Color;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mode = "Прямоугольник";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            mode = "Эллипс";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            mode = "Карандаш";
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (mode == "Прямоугольник")
+            {
+                Graphics g;
+                g = Graphics.FromImage(pic);
+                Pen pencil;
+                pencil = new Pen(button1.BackColor, trackBar1.Value);
+                int caseSwitch = -1;
+                if ((xclick1 > e.X) && (yclick1 > e.Y)) caseSwitch = 0;
+                if ((xclick1 > e.X) && (yclick1 < e.Y)) caseSwitch = 1;
+                if ((xclick1 < e.X) && (yclick1 > e.Y)) caseSwitch = 2;
+                switch (caseSwitch) {
+                    case 0:
+                        g.DrawRectangle(pencil, e.X, e.Y, xclick1 - e.X, yclick1 - e.Y);
+                        break;
+                    case 1:
+                        g.DrawRectangle(pencil, e.X, yclick1, xclick1 - e.X, e.Y - yclick1);
+                        break;
+                    case 2:
+                        g.DrawRectangle(pencil, xclick1, e.Y, e.X - xclick1, yclick1 - e.Y);
+                        break;
+                    default:
+                        g.DrawRectangle(pencil, xclick1, yclick1, e.X - xclick1, e.Y - yclick1);
+                        break;
+                }
+
+            }
+            if (mode == "Эллипс")
+            {
+                Graphics g;
+                g = Graphics.FromImage(pic);
+                Pen pencil;
+                pencil = new Pen(button1.BackColor, trackBar1.Value);
+                g.DrawEllipse(pencil, xclick1, yclick1, e.X - xclick1, e.Y - yclick1);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            mode = "Рука";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            mode = "Масштабирование";
+        }
+
+        private void оПрограммеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Версия программы 1.00\nАвтор: Луцик Александр Николаевич", "О программе");
+        }
+
+        private void выйтиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (mode == "Масштабирование")
+            {
+                var brush = new SolidBrush(Color.White);
+                var s_pic = new Bitmap((int)width, (int)height);
+                var s_g = Graphics.FromImage(s_pic);
+                if ((e.Button == MouseButtons.Right) && (c_c > 0))
+                {
+                    scale /= 2;
+                    c_c -= 1;
+                }
+                if ((e.Button == MouseButtons.Left) && (c_c < 8))
+                {
+                    scale *= 2;
+                    c_c += 1;
+                }
+                if ((c_c >= 0) && (c_c <= 8))
+                {
+                    var scaleWidth = (int)(pic.Width * scale);
+                    var scaleHeight = (int)(pic.Height * scale);
+                    s_g.FillRectangle(brush, new RectangleF(0, 0, (float)(width), (float)(height)));
+                    s_g.DrawImage(pic, ((int)width - scaleWidth) / 2, ((int)height - scaleHeight) / 2, scaleWidth, scaleHeight);
+                    s_g.DrawImage(pic, ((int)width - scaleWidth) / 2, ((int)height - scaleHeight) / 2, scaleWidth, scaleHeight);
+                    pictureBox1.Image = s_pic;
+                }
+            }
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            xclick1 = e.X;
+            yclick1 = e.Y;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mode != "Масштабирование")
+            {
+                Pen pencil;
+                pencil = new Pen(button1.BackColor, trackBar1.Value);
+                pencil.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+                pencil.StartCap = System.Drawing.Drawing2D.LineCap.Round;
+                Graphics g;
+                g = Graphics.FromImage(pic);
+                Graphics t_g;
+                t_g = Graphics.FromImage(t_pic);
+                if (e.Button == MouseButtons.Left)
+                {
+
+                    if (mode == "Карандаш")
+                    {
+                        g.DrawLine(pencil, x1, y1, e.X, e.Y);
+                        pictureBox1.Image = pic;
+                    }
+                    if (mode == "Прямоугольник")
+                    {
+                        t_g.Clear(Color.White);
+                        int x = xclick1, y = yclick1;
+                        if (x > e.X) x = e.X;
+                        if (y > e.Y) y = e.Y;
+                        t_g.DrawRectangle(pencil, x, y, Math.Abs(e.X - xclick1), Math.Abs(e.Y - yclick1));
+                        t_g.DrawImage(pic, 0, 0);
+                        pictureBox1.Image = t_pic;
+                    }
+                    if (mode == "Эллипс")
+                    {
+                        t_g.Clear(Color.White);
+                        t_g.DrawEllipse(pencil, xclick1, yclick1, e.X - xclick1, e.Y - yclick1);
+                        t_g.DrawImage(pic, 0, 0);
+                        pictureBox1.Image = t_pic;
+                    }
+                }
+                x1 = e.X;
+                y1 = e.Y;
+            }
+        }
+    }
+}
